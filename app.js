789feeb1e384c4d1e485ccee481bd0888c8cd96d
SAHIL BAIDYA <96975851+sahilbaidya22@users.noreply.github.com>
2022-01-08 19:28:37 +0530

    w-e message desired

---
+++ app.js
@@ -0,0 +1,13 @@
+import React from "react";
+// import Todo from "./component/Todo-list/todo";
+import Temp from "./component/weather/temp";
+
+const App = () => {
+  return (
+    <>
+      <Temp />
+    </>
+  );
+};
+
+export default App;
